768ff14b539b27d8b6659f408ef94d892a6e88b3
yms <yms@DESKTOP-7T9O25Q>
2017-11-15 23:19:25 +0900

    array method, not array method

--- src/CalendarMain.java
+++ src/CalendarMain.java
@@ -2,33 +2,13 @@ import java.util.Scanner;
 
 public class CalendarMain {
 	
-	public static void main(String []args) {
-		
-		//ctrl + shift + f
-		System.out.println("월    화    수    목    금    토    일");
-		System.out.println("---------------------");
-		System.out.println("1  2  3  4  5  6  7");
-		System.out.println("8  9  10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");		
-		
-		//utf-8 인코딩 window -> preference -> general -> workspace -> utf-8
-		
-		Scanner scan = new Scanner(System.in);
-		System.out.println();
-		System.out.println("달을 입력하세요.");
-		int month = scan.nextInt();
-//		if(month > 12) System.out.println("1~12 까지의 숫자를 입력해주세요");
-//		else System.out.println(month+"월은 "+returnDay(month)+"일까지 있습니다.");
-		
-		int [] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		System.out.println(month+"월은 " + maxDays[month - 1]+ "일까지 있습니다.");
-		
-		scan.close();
-		
+	private static final int [] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int useArrayGetMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month - 1];
 	}
 	
-	public static int returnDay(int month) {
+	public int getMaxDaysOfMonth(int month) {
 		int day = 0;
 		if(month>0 && month <8) {
 			if(month == 2) {
@@ -43,5 +23,33 @@ public class CalendarMain {
 		}
 		return day;
 	}
+	
+	public static void main(String []args) {
+		
+		//ctrl + shift + f
+		System.out.println("월    화    수    목    금    토    일");
+		System.out.println("---------------------");
+		System.out.println("1  2  3  4  5  6  7");
+		System.out.println("8  9  10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");		
+		
+		//utf-8 인코딩 window -> preference -> general -> workspace -> utf-8
+		
+		Scanner scan = new Scanner(System.in);
+		CalendarMain cal = new CalendarMain();
+		System.out.println();
+		System.out.println("달을 입력하세요.");
+		int month = scan.nextInt();
+//		if(month > 12) System.out.println("1~12 까지의 숫자를 입력해주세요");
+//		else System.out.println(month+"월은 "+returnDay(month)+"일까지 있습니다.");
+		
+		System.out.println(month+"월은 " + cal.useArrayGetMaxDaysOfMonth(month)+ "일까지 있습니다.");
+		
+		scan.close();
+		
+	}
+	
+
 
 }
