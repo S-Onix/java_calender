9984531c5a53a8692c17b6ca794b4523b33c077a
yms <yms@DESKTOP-7T9O25Q>
2017-11-15 22:26:48 +0900

    Sum example

--- src/CalendarMain.java
+++ src/CalendarMain.java
@@ -1,17 +1,19 @@
+import java.util.Scanner;
 
 public class CalendarMain {
 	
-	public static void main(String []args) {
-		
-		//ctrl + shift + f
-		System.out.println("월    화    수    목    금    토    일");
-		System.out.println("---------------------");
-		System.out.println("1  2  3  4  5  6  7");
-		System.out.println("8  9  10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");		
-		
-		//utf-8 인코딩 window -> preference -> general -> workspace -> utf-8 ���� ����
-	}
+//	public static void main(String []args) {
+//		
+//		//ctrl + shift + f
+//		System.out.println("월    화    수    목    금    토    일");
+//		System.out.println("---------------------");
+//		System.out.println("1  2  3  4  5  6  7");
+//		System.out.println("8  9  10 11 12 13 14");
+//		System.out.println("15 16 17 18 19 20 21");
+//		System.out.println("22 23 24 25 26 27 28");		
+//		
+//		//utf-8 인코딩 window -> preference -> general -> workspace -> utf-8
+//		
+//	}
 
 }
---
+++ src/SUm.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SUm {
+	public static void main(String[] args) {
+		//ctrl + shift + n/o  <-- 자동 import
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		
+		String s1, s2;
+		int number1 , number2;
+		
+		System.out.println("두 수를 입력하세요");
+		
+		
+		s1 = scan.next();
+		s2 = scan.next();
+		
+		number1 = Integer.parseInt(s1);
+		number2 = Integer.parseInt(s2);
+		
+		//숫자를 바로 입력
+//		number1 = scan.nextInt();
+//		number2 = scan.nextInt();
+		
+		System.out.println("두 수의 합은 " + (number1+number2) + "입니다.");
+	}
+}
