413d87c09659643f94854121e1fa8af813981702
yms <yms@DESKTOP-7T9O25Q>
2017-11-15 23:22:24 +0900

    Over month solve

--- src/CalendarMain.java
+++ src/CalendarMain.java
@@ -24,27 +24,30 @@ public class CalendarMain {
 		return day;
 	}
 	
-	public static void main(String []args) {
-		
-		//ctrl + shift + f
+	public void printCalendar() {
 		System.out.println("월    화    수    목    금    토    일");
 		System.out.println("---------------------");
 		System.out.println("1  2  3  4  5  6  7");
 		System.out.println("8  9  10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");		
+		System.out.println("22 23 24 25 26 27 28");	
+		System.out.println();
+	}
+	
+	public static void main(String []args) {
+		
+		CalendarMain cal = new CalendarMain();
+		Scanner scan = new Scanner(System.in);
 		
 		//utf-8 인코딩 window -> preference -> general -> workspace -> utf-8
 		
-		Scanner scan = new Scanner(System.in);
-		CalendarMain cal = new CalendarMain();
-		System.out.println();
-		System.out.println("달을 입력하세요.");
-		int month = scan.nextInt();
-//		if(month > 12) System.out.println("1~12 까지의 숫자를 입력해주세요");
-//		else System.out.println(month+"월은 "+returnDay(month)+"일까지 있습니다.");
+		cal.printCalendar();
 		
-		System.out.println(month+"월은 " + cal.useArrayGetMaxDaysOfMonth(month)+ "일까지 있습니다.");
+		
+		System.out.println("달을 입력하세요.");
+		int month = scan.nextInt();	
+		if(month > 12) System.out.println("1~12 까지의 숫자만 입력해주세요.");
+		else System.out.println(month+"월은 " + cal.useArrayGetMaxDaysOfMonth(month)+ "일까지 있습니다.");
 		
 		scan.close();
 		
