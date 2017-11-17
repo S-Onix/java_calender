0f25e4cd8b711c2730488e1533069c4c3b0acbbd
yms <yms@DESKTOP-7T9O25Q>
2017-11-17 19:52:04 +0900

    Implement Automatic Calendar

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,14 +1,14 @@
 public class Calendar {
 
-	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final String[] FIRST_DAY = { "SU", "MO", "TU", "WE", "TH", "FR", "SA" };
 
 	public int useArrayGetMaxDaysOfMonth(int year, int month) {
 		if (isLeapYear(year))
-			return LEAP_MAX_DAYS[month - 1];
+			return LEAP_MAX_DAYS[month];
 		else
-			return MAX_DAYS[month - 1];
+			return MAX_DAYS[month];
 	}
 
 	public int getMaxDaysOfMonth(int month) {
@@ -35,34 +35,72 @@ public class Calendar {
 			return false;
 	}
 
-	public int isFirstOfDay(String weekend) {
-		int startDayPoint;
+	// public int isFirstOfDay(String weekend) {
+	// int startDayPoint;
+	//
+	// for (int i = 0; i < FIRST_DAY.length; i++) {
+	// if (FIRST_DAY[i].equals(weekend)) {
+	// startDayPoint = i;
+	// return startDayPoint;
+	// }
+	// }
+	//
+	// return -1;
+	// }
 
-		for (int i = 0; i < FIRST_DAY.length; i++) {
-			if (FIRST_DAY[i].equals(weekend)) {
-				startDayPoint = i;
-				return startDayPoint;
-			}
+	/**
+	 * 1. 기준날짜를 잡는다 
+	 * 2. 기준날짜 ~ 현재 날짜까지의 일수를 구한다 
+	 * 3. 기준요일 + 계산된 날짜 % 7 을 통해 그 달 첫째날을 구한다
+	 * 
+	 */
+
+	private int getWeekDay(int year, int month, int day) {
+		// 기준 1970년 달력 1970년 1월 1일 은 목요일
+		int syear = 1970;
+		final int STANDARD_WEEKDAY = 3;
+
+		int delta;
+
+		// 356, 355
+
+		int count = 0;
+
+		for (int i = syear; i < year; i++) {
+			delta = isLeapYear(i) ? 366 : 365;
+			count += delta;
 		}
 
-		return -1;
+		for (int i = 1; i < month; i++) {
+			delta = useArrayGetMaxDaysOfMonth(year, i);
+			count += delta;
+
+		}
+
+		count += day;
+
+		int weekday = (count + STANDARD_WEEKDAY) % 7;
+
+		return weekday;
 	}
 
-	public void printCalendar(int year, int month, String startWeekend) {
+	public void printCalendar(int year, int month) {
 
 		int MaxDay = useArrayGetMaxDaysOfMonth(year, month);
-		int startWeekendOfNumber = isFirstOfDay(startWeekend);
+
+		// get weekday automatically
+		int startWeekend = getWeekDay(year, month, 1);
 
 		System.out.printf("   <<%4d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("----------------------");
 
-		for (int i = 0; i < startWeekendOfNumber; i++) {
+		for (int i = 0; i < startWeekend; i++) {
 			System.out.printf("   ");
 		}
 
 		for (int i = 1; i < MaxDay + 1; i++) {
-			if ((i + startWeekendOfNumber) % 7 == 0) {
+			if ((i + startWeekend) % 7 == 0) {
 				System.out.printf("%3d\n", i);
 			} else {
 				System.out.printf("%3d", i);
--- src/Prompt.java
+++ src/Prompt.java
@@ -1,32 +1,46 @@
 import java.util.Scanner;
 
 public class Prompt {
-	
-	//변경이 없을때 final 사용
+
+	// 변경이 없을때 final 사용
 	private Scanner scan = new Scanner(System.in);
 	private int month = 0;
 	private int year = 2017;
-	private String weekend;
 	private Calendar cal = new Calendar();
-	
+
 	public void runPrompt() {
 		while (true) {
-			System.out.println("년도를 입력하세요.");
+			System.out.println("년도를 입력하세요.(exit : -1)");
 			System.out.printf("YEAR> ");
 			year = scan.nextInt();
+			
+			if(year == -1) {
+				break;
+			}
+			
 			System.out.println("월을 입력하세요.");
 			System.out.printf("MONTH> ");
 			month = scan.nextInt();
-			System.out.println("시작 요일을 선택하세요 (\"SU\", \"MO\", \"TU\",, \"WE\", \"TH\", \"FR\", \"SA\")중 하나를 입력해주세요");
-			System.out.printf("WEEKEND> ");
-			weekend = scan.next();
 			
-			cal.printCalendar(year, month, weekend);
+			if (month > 12 || month < 1) {
+				System.err.println("잘못 입력하셨습니다");
+				continue;
+			}
 			
+//			System.out.println("시작 요일을 선택하세요 (\"SU\", \"MO\", \"TU\",, \"WE\", \"TH\", \"FR\", \"SA\")중 하나를 입력해주세요");
+//			System.out.printf("WEEKEND> ");
+//			weekend = scan.next();
+
+			
+
+//			cal.printCalendar(year, month, weekend);
+			
+			cal.printCalendar(year, month);
+
 		}
 	}
-	
-	public static void main(String []args) {
+
+	public static void main(String[] args) {
 		Prompt main = new Prompt();
 		main.runPrompt();
 	}
